583e7cd91928ce2127f01e7fdf78e74701ea9d09
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-24 17:51:59 +0530

    Create Product rating and price in curated_items.dart

--- lib/Views/Widgets/curated_items.dart
+++ lib/Views/Widgets/curated_items.dart
@@ -39,7 +39,67 @@ class CuratedItems extends StatelessWidget {
           ),
         ),
         SizedBox(height: 7),
-        
+        Row(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "H&M",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.black26,
+              ),
+            ),
+            SizedBox(width: 5),
+            Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 17,
+            ),
+            Text(eCommerceItems.rating.toString()),
+            Text(
+              "(${eCommerceItems.review})",
+              style: const TextStyle(
+                color: Colors.black26,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: size.width * 0.5,
+          child: Text(
+            eCommerceItems.name,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+              height: 1.5,
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Text(
+              "\$${eCommerceItems.price.toString()}.00",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+                color: Colors.red,
+                height: 1.5,
+              ),
+            ),
+            SizedBox(width: 5),
+            if (eCommerceItems.isCheck == true)
+              Text(
+                "\$${eCommerceItems.price + 255}.00",
+                style: TextStyle(
+                  color: Colors.black26,
+                  decoration: TextDecoration.lineThrough,
+                  decorationColor: Colors.black26,
+                ),
+              ),
+          ],
+        ),
       ],
     );
   }
