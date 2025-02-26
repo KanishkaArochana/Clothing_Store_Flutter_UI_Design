1b94198120e90d94c83146df02d17c8b6264e380
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-02-26 10:04:18 +0530

    Create a sub_category.dart

---
+++ lib/Model/sub_category.dart
@@ -0,0 +1,33 @@
+class SubCategory {
+  final String name;
+  final String image;
+
+  SubCategory({required this.name, required this.image});
+}
+
+List<SubCategory> subcategory = [
+  SubCategory(
+    name: "Bags",
+    image: "assets/sub_category/image6.png",
+  ),
+  SubCategory(
+    name: "Wallets",
+    image: "assets/sub_category/image3.png",
+  ),
+  SubCategory(
+    name: "Footwear",
+    image: "assets/sub_category/image4.png",
+  ),
+  SubCategory(
+    name: "Clothes",
+    image: "assets/sub_category/image5.png",
+  ),
+  SubCategory(
+    name: "Watch",
+    image: "assets/sub_category/image1.png",
+  ),
+  SubCategory(
+    name: "Makeup",
+    image: "assets/sub_category/image2.png",
+  ),
+];
